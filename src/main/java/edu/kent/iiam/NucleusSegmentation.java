a42044986bca26ae50cbac7f95223a502311497d
Rajan Maharjan <rmaharj2@kent.edu>
2021-07-16 13:47:21 -0400

    feat: added ScriptService to run macro

--- src/main/java/edu/kent/iiam/NucleusSegmentation.java
+++ src/main/java/edu/kent/iiam/NucleusSegmentation.java
@@ -8,85 +8,57 @@
 
 package edu.kent.iiam;
 
-import net.imagej.Dataset;
 import net.imagej.ImageJ;
-import net.imagej.ops.OpService;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.command.Command;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.ui.UIService;
+import org.scijava.script.ScriptService;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileNotFoundException;
+import javax.script.ScriptException;
+
 
 
 @Plugin(type = Command.class, menuPath = "Plugins>NucleusSegmentation")
 public class NucleusSegmentation<T extends RealType<T>> implements Command {
-    //
-    // Feel free to add more parameters here...
-    //
 
-    @Parameter
-    private Dataset currentData;
 
-    @Parameter
-    private UIService uiService;
+	@Parameter
+	private LogService logService;
 
-    @Parameter
-    private OpService opService;
+	@Parameter
+	private File macro_file;
 
-    @Override
-    public void run() {
-        final Img<T> image = (Img<T>)currentData.getImgPlus();
+	@Parameter
+	private ScriptService scriptService;
 
-        //
-        // Enter image processing code here ...
-        // The following is just a Gauss filtering example
-        //
-        final double[] sigmas = {1.0, 3.0, 5.0};
+	@Override
+	public void run()  {
+		try {
+			scriptService.run(macro_file, true);
+		} catch (FileNotFoundException | ScriptException e) {
+			// TODO Auto-generated catch block
+			logService.error(e.toString());
+		}
 
-        List<RandomAccessibleInterval<T>> results = new ArrayList<>();
+	}
 
-        for (double sigma : sigmas) {
-            results.add(opService.filter().gauss(image, sigma));
-        }
+	/**
+	 * This main function serves for development purposes. It allows you to run the
+	 * plugin immediately out of your integrated development environment (IDE).
+	 *
+	 * @param args whatever, it's ignored
+	 * @throws Exception
+	 */
+	public static void main(final String... args) throws Exception {
+		// create the ImageJ application context with all available services
+		final ImageJ ij = new ImageJ();
+		// ij.ui().showUI();
 
-        // display result
-        for (RandomAccessibleInterval<T> elem : results) {
-            uiService.show(elem);
-        }
-    }
-
-    /**
-     * This main function serves for development purposes.
-     * It allows you to run the plugin immediately out of
-     * your integrated development environment (IDE).
-     *
-     * @param args whatever, it's ignored
-     * @throws Exception
-     */
-    public static void main(final String... args) throws Exception {
-        // create the ImageJ application context with all available services
-        final ImageJ ij = new ImageJ();
-        ij.ui().showUI();
-
-        // ask the user for a file to open
-        final File file = ij.ui().chooseFile(null, "open");
-
-        if (file != null) {
-            // load the dataset
-            final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
-
-            // show the image
-            ij.ui().show(dataset);
-
-            // invoke the plugin
-            ij.command().run(NucleusSegmentation.class, true);
-        }
-    }
+		ij.command().run(NucleusSegmentation.class, true);
+	}
 
 }
