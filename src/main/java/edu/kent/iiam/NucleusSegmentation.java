70bf89b57319276b83dfb391ec93c33f9f5bb1d0
Rajan Maharjan <rmaharj2@kent.edu>
2021-07-16 14:41:22 -0400

    feat: added ScriptService to run macro from local directory

--- src/main/java/edu/kent/iiam/NucleusSegmentation.java
+++ src/main/java/edu/kent/iiam/NucleusSegmentation.java
@@ -8,41 +8,90 @@
 
 package edu.kent.iiam;
 
+import net.imagej.Dataset;
 import net.imagej.ImageJ;
+import net.imagej.ops.OpService;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.script.ScriptService;
+import org.scijava.ui.UIService;
+
+import net.imagej.ImageJ;
+import net.imglib2.algorithm.labeling.ConnectedComponents;
+import net.imglib2.img.Img;
+import net.imglib2.roi.labeling.ImgLabeling;
+import net.imglib2.roi.labeling.LabelRegions;
+import net.imglib2.type.numeric.IntegerType;
+
+import ij.IJ;
+import ij.ImagePlus;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.print.DocFlavor.URL;
 import javax.script.ScriptException;
-
-
 
 @Plugin(type = Command.class, menuPath = "Plugins>NucleusSegmentation")
 public class NucleusSegmentation<T extends RealType<T>> implements Command {
 
-
+	// @Parameter(label = "Sigma:", description = "Sigma value for GaussFilter.")
+	// private static double sigma = 1.2;
 	@Parameter
 	private LogService logService;
 
-	@Parameter
-	private File macro_file;
+	// @Parameter
+	// private File macro_file;
 
 	@Parameter
 	private ScriptService scriptService;
 
 	@Override
-	public void run()  {
-		try {
-			scriptService.run(macro_file, true);
-		} catch (FileNotFoundException | ScriptException e) {
-			// TODO Auto-generated catch block
-			logService.error(e.toString());
+	public void run() {
+		ImageJ ij = new ImageJ();
+
+		System.out.println("Start NucleusSegmentation... ");
+
+		java.net.URL resource = getClass().getClassLoader().getResource("Macro_12.ijm");
+
+		if (resource == null) {
+			throw new IllegalArgumentException("file not found!");
+		} else {
+
+			// failed if files have whitespaces or special characters
+			File macro_file =  new File(resource.getFile());
+			// return new File(resource.toURI());
+			System.out.println(macro_file);
+			try {
+				scriptService.run(macro_file , true);
+			} catch (FileNotFoundException | ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
+		//		File macro_file;
+		//		try {
+		//			macro_file = (File) ij.io().open(Object.class.getResource("./Macro_12.ijm").getPath());
+		//			System.out.println("Counted " + macro_file);
+		//
+		////				scriptService.run(macro_file , true);
+		//		} catch (IOException e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//			logService.error(e.toString());
+		//			System.out.println("Start NucleusSegmentation... "+ e);
+		//			System.out.println("Start NucleusSegmentation... "+ e.toString());
+		//
+		//		};
 
 	}
 
@@ -56,9 +105,8 @@ public class NucleusSegmentation<T extends RealType<T>> implements Command {
 	public static void main(final String... args) throws Exception {
 		// create the ImageJ application context with all available services
 		final ImageJ ij = new ImageJ();
-		// ij.ui().showUI();
-
 		ij.command().run(NucleusSegmentation.class, true);
+		System.out.println("Processing NucleusSegmentation...");
 	}
 
 }
